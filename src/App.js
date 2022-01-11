562d0b7257176967a298927956772083560b1b9d
simonchrist <simonchristian299@yahoo.com>
2022-01-11 17:21:42 -0500

    finished city-search function based off zip-search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [apiResponse, allResponses] = useState([]);
+
+  const getResponse = async () => {
+    let cityname = document.getElementById("city").value;
+
+    await axios
+      .get('http://ctp-zip-api.herokuapp.com/city/' + cityname)
+      .then(response => {
+        console.log(response.data);
+        allResponses(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.data.error);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className='title'>City Search</h1>
+      <h2>Enter City Name in Captitals</h2>
+        <label htmlFor="city" className="input"><b>City Name:</b></label>
+          <div>
+            <input type="text" id="city" className="form" onChange={getResponse}/>
+      </div>
+        <div className='box1'><h1>Zipcodes in this City:</h1></div>
+            <ul>
+              {
+                apiResponse.map((event, i) => {
+                return event === "Nothing Found" ? <p key={i}>Nothing Found</p> :
+                   <li key={i}>{event}</li>
+                })
+              }
+            </ul>
+        </div>
   );
 }
 
